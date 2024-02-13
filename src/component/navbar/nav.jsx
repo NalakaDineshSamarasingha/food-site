868e387d09c54a715a03608a8c7016f217f03313
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-02-13 12:08:17 +0530

    add navbar for mobo screen

--- src/component/navbar/nav.jsx
+++ src/component/navbar/nav.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './nav.css';
 import Logo from "../assests/logo.png";
 
 const Navbar = ()=>{
+    const [showMenu,setShowMenu] = useState(false);
     return(
+
         <div className="navbar">
             <div className="container">
                 <div className="logo">
                     <img src={Logo} alt="Logo" className="logo"/>
                 </div>
-                <div className="navlinks">
-                    <ul className="link">
-                        <li className="home">Home</li>
-                        <li className="food">Food</li>
-                        <li className="about">About Us</li>
-                        <li className="contact">Contact Us</li>
+                <div className="navlinks" style={{display: showMenu?'flex':'none'}}>
+                    <ul className="link" >
+                        <li className="home" onClick={()=>setShowMenu(false)} >Home</li>
+                        <li className="food" onClick={()=>setShowMenu(false)} >Food</li>
+                        <li className="about" onClick={()=>setShowMenu(false)} >About Us</li>
+                        <li className="contact" onClick={()=>setShowMenu(false)} >Contact Us</li>
                     </ul>
                 </div>
                 <div className="order-button">
                     <button className="btn">Order Now</button>
                 </div>
+                <p className="mobo"  onClick={()=> setShowMenu(!showMenu)}>&#9776;</p>
             </div>
         </div>
     )
