d7745ff2d680669d0bc43d3269bc023c03da1fe6
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-02-13 10:28:10 +0530

    Add some change in header component

--- src/component/Header/header.jsx
+++ src/component/Header/header.jsx
@@ -3,15 +3,19 @@ import './header.css';
 import Header_img from '../assests/header.png'
 const Header = () => {
     return(
-        <div className="header">
+        <div className="c">
+            <div className="header">
             <div className="left">
+                <span className="hello" >Hello Foodies,</span>
                 <p className="header-text">DELCICIOUS <br /> HEALTHY FOOD</p>
                 <button className="btn">Order Now</button>
             </div>
             <div className="right">
-                <img src={Header_img} alt="Header" style={{width:"800px"}} />
+                <img src={Header_img} alt="Header"  />
             </div>
         </div>
+        </div>
+        
     )
 }
 
